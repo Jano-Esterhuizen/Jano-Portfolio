2e75428444ee4293cadb6eb2d48cb095b5caf92f
Jano Personal <esterhuizenjano@gmail.com>
2024-10-22 22:17:41 +0200

    Created Contact Form
    - Used https://getform.io/ for recieving message

--- Jano-Portfolio/src/App.jsx
+++ Jano-Portfolio/src/App.jsx
@@ -4,6 +4,7 @@
  */
 
 import About from "./components/About"
+import Contact from "./components/Contact"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Review from "./components/Review"
@@ -21,6 +22,7 @@ const App = () => {
         <Skill/>
         <Work/>
         <Review/>
+        <Contact/>
       </main>
     </>
   )
