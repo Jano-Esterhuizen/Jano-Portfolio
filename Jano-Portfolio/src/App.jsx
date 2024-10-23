7c4890499788a0c33ef0a4ad46198fe7b179efcb
Jano Personal <esterhuizenjano@gmail.com>
2024-10-23 13:27:15 +0200

    Created Footer Section

--- Jano-Portfolio/src/App.jsx
+++ Jano-Portfolio/src/App.jsx
@@ -5,6 +5,7 @@
 
 import About from "./components/About"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Review from "./components/Review"
@@ -23,6 +24,7 @@ const App = () => {
         <Work/>
         <Review/>
         <Contact/>
+        <Footer/>
       </main>
     </>
   )
